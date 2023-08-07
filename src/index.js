2b14e24dd0cc135f0b2eb99cde80aeee49a36444
Andresr35 <andresjruiz@cpp.edu>
2023-08-07 11:59:24 -0700

    start to convert html to js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,36 @@
 import 'normalize.css';
 import './style.css';
 import Pizza from './pizaa.jpeg';
+
+const nav = ()=>{
+    const navElement = document.createElement('nav');
+
+    const h1 = document.createElement('h1');
+    h1.textContent = "Andres' Pizza Company"
+
+    const ul = document.createElement('ul');
+
+    const home = document.createElement('li'); 
+    home.textContent= "Home";
+    const about = document.createElement('li'); 
+    about.textContent= "About";
+    const contact = document.createElement('li'); 
+    contact.textContent= "Contact Us";
+
+    ul.appendChild(home);
+    ul.appendChild(about);
+    ul.appendChild(contact);
+    navElement.appendChild(h1);
+    navElement.appendChild(ul);
+
+    return navElement;
+}
+
+function render(){
+    const content = document.querySelector('body');
+
+    content.appendChild(nav());
+}
+
+
+render();
